6bd7066407c2551205eafec62736794d55c6a69e
Aryak Sen <34271412+Aryak320@users.noreply.github.com>
2026-03-09 01:02:42 +0100

    newer additions

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,7 +5,7 @@ return {
   --     -- load the dayfox variant
   --     require("nightfox").setup({
   --       options = {
-  --         transparent = false,
+  --         transparent = true,
   --       },
   --     })
   --     -- make the insert-mode cursor a black vertical line
@@ -21,12 +21,12 @@ return {
   --     -- ]])
   --   end,
   -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
+  -- -- {
+  -- --   "LazyVim/LazyVim",
+  -- --   opts = {
+  -- --     colorscheme = "catppuccin-mocha",
+  -- --   },
+  -- -- },
   {
     "folke/which-key.nvim",
     opts = {
@@ -71,12 +71,17 @@ return {
       }
 
       -- Indentation settings
-      vim.g.vimtex_indent_enabled = false
-      vim.g.tex_indent_items = false
-      vim.g.tex_indent_brace = false
+      vim.g.vimtex_indent_enabled = true
+      vim.g.tex_indent_items = true
+      vim.g.tex_indent_brace = true
 
       -- Suppression settings
       vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_quickfix_ignore_filters = {
+        [[Underfull \\hbox.*]], -- hide underfull hbox warnings
+        [[Overfull \\hbox.*]], -- (optional) hide overfull hbox warnings too
+        [[Underfull \\vbox.*]], -- (optional) hide underfull vbox warnings too
+      }
       vim.g.vimtex_log_ignore = {
         "Underfull",
         "Overfull",
@@ -101,15 +106,81 @@ return {
   },
 
   {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "texlab",
+      },
+    },
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = { "markdown" },
+      ensure_installed = { "all" },
       highlight = {
         enable = true,
         disable = { "latex" },
         additional_vim_regex_highlighting = { "latex", "markdown" },
       },
       -- other treesitter settings
+    },
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        texlab = {
+          settings = {
+            texlab = {
+              diagnostics = {
+                { virtual_text = false },
+                ignoredPatterns = {
+                  -- Rust regexes. Match the diagnostic text you want to suppress.
+                  [[Underfull \\hbox.*]],
+                  [[Overfull \\hbox.*]],
+                  -- If you want to be extra broad:
+                  -- [[Underfull \\hbox.*]],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = {
+        indent = { enabled = false },
+        chunk = {
+          -- when enabled, scopes will be rendered as chunks, except for the
+          -- top-level scope which will be rendered as a scope.
+          enabled = true,
+          -- only show chunk scopes in the current window
+          only_current = true,
+          priority = 1,
+          hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+          char = {
+            --corner_top = "┌",
+            --corner_bottom = "└",
+            corner_top = "╭",
+            corner_bottom = "╰",
+            horizontal = "─",
+            vertical = "│",
+            arrow = ">",
+          },
+        },
+      },
     },
   },
 }
